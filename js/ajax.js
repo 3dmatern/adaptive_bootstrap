07b63cef29bf592f725e48091578eb7f3264dc19
Denis <110766239+3dmatern@users.noreply.github.com>
2022-11-22 23:11:13 +0300

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,49 +1,29 @@
-$( document ).ready(function() {
-    $("#ajax_btn").click(
-		function(){
-			sendAjaxForm('result_form', 'ajax_form', 'php/action_ajax_form.php');
-			return false; 
-		}
-	);
+$(document).ready(function() {
+    $('form').submit(function(e){
+        e.preventDefault(); //запрещает переход на файл указанный в атрибуте action (запрещает отправку формы в браузере)
+        let data = $("form").serializeArray();
+        
+        $.ajax({
+            type: $(this).attr('method'),
+            url: $(this).attr('action'), //this - получение данных! В данном случае из attr(Атрибута) action т.е. в данном случае путь к файлу
+            data: data, // Получение отправляемых данных из всей формы
+            cache: false,
+            //processData: false,  //чтобы данные не преобразовывались в строку
+            success: function(msg){
+                console.log(msg);
+                if(msg == 'OK') {
+                    $('form').find('input[type=text], input[type=email], input[type=tel]').val(''); //очищает input полсе отправки
+                    result = 'Данные отправлены! <b>Мы скоро Вам перезвоним!</b>'
+                } else {
+                    result = msg;
+                }
+                $("#result_form").html(result);
+                $("#result_form_2").html(result);
+            },
+            error: function(){ //данная ошибка выводится, если мы в скрипте указали например неверный путь к php обработчику
+                $("#result_form").html('Ошибка отпраки данных!');
+            }
+        })
+        return false;
+    });
 });
- 
-function sendAjaxForm(result_form, ajax_form, url) {
-    $.ajax({
-        url:     url, //url страницы (action_ajax_form.php)
-        type:     "POST", //метод отправки
-        dataType: "html", //формат данных
-        data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
-        success: function(response) { //Данные отправлены успешно
-        	result = $.parseJSON(response);
-        	$('#result_form').html('Спасибо, ждите новостей!');
-    	},
-    	error: function(response) { // Данные не отправлены
-            $('#result_form').html('Ошибка. Данные не отправлены.');
-    	}
- 	});
-};
-
-$( document ).ready(function() {
-    $("#ajax_btn_2").click(
-		function(){
-			sendAjaxForm('result_form_2', 'ajax_form_2', 'php/action_ajax_form.php');
-			return false; 
-		}
-	);
-});
- 
-function sendAjaxForm(result_form_2, ajax_form_2, url) {
-    $.ajax({
-        url:     url, //url страницы (action_ajax_form.php)
-        type:     "POST", //метод отправки
-        dataType: "html", //формат данных
-        data: $("#"+ajax_form_2).serialize(),  // Сеарилизуем объект
-        success: function(response) { //Данные отправлены успешно
-        	result = $.parseJSON(response);
-        	$('#result_form_2').html('Спасибо, ждите новостей!');
-    	},
-    	error: function(response) { // Данные не отправлены
-            $('#result_form_2').html('Ошибка. Данные не отправлены.');
-    	}
- 	});
-};
